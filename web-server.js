3b0da40fbf92f4b664858c06d0e5836e6d00f92c
Ítalo Andrade <andraditalo@gmail.com>
2017-10-08 18:41:20 -0300

    -

--- web-server.js
+++ web-server.js
@@ -13,10 +13,7 @@ function loadServer() {
     const app = express();
 
     app.use((req, res, next) => {
-        res.header('Content-Type', 'application/json; charset=utf-8');
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authentication");
 
         if (req.method === 'OPTIONS')
             return res.status(204).end();
